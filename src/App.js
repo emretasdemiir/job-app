2b95d23820172fe2f1e414b38692a5e5119c5fc0
emre.tasdemir <emre.tasdemir@kocholding.onmicrosoft.com>
2022-04-23 18:24:28 +0300

    footer-added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Layout from "./components/Layout/Layout";
 import './App.css';
 import logo from './logo.jpg';
+import { Footer } from "antd/lib/layout/layout";
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
       </header>
       <Layout />
+      <footer className="App-footer">
+        <p className="footer-text">&copy; 2022 Emre Taşdemir</p>
+      </footer>
     </div>
   );
 }
